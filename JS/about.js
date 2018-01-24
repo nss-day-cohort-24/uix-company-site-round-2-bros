c74d2ad821c86d9c1add933ce013c692cceced1d
Arthur Smith <mirandasimpson@Mirandas-MacBook-Air.local>
2018-01-24 09:04:48 -0600

    i are done

---
+++ JS/about.js
@@ -0,0 +1,11 @@
+const arthur = document.getElementById("arthur");
+arthur.innerHTML = `Arthur Smith`;
+
+const amber = document.getElementById("amber");
+amber.innerHTML = `Amber Sharpe`;
+
+const alexis = document.getElementById("alexis");
+alexis.innerHTML = `Alexis Hennecy`;
+
+const taylor = document.getElementById("taylor");
+taylor.innerHTML = `Taylor Dotson`;
