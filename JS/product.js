e1040ade4fa39583da9e576cb8edfd511d3f104c
taylordotson <33382665+taylordotson@users.noreply.github.com>
2018-01-24 11:58:48 -0600

    added logo

--- JS/product.js
+++ JS/product.js
@@ -1,35 +1,23 @@
 var products = [
-    {name: "Dress to Impress", price: "$100", image: "images/suit.jpg", id:1 },
-    {name: "Confidence Coaching", price: "$100", image: "images/confidence.jpg", id:2},
-    {name: "Match-making", price: "$100", image: "images/matchmaking.jpg", id:3 },
-    {name: "Financial Advice", price: "$100", image: "images/money.jpg", id:4},
-    {name: "Networking/Mentoring", price: "$100" , image: "images/networking.jpg", id:5},
-    {name: "Shaving Kit", price: "$100", image: "images/shaving.jpg", id:6},
-    {name: "Give Back", price: "$0", image: "images/volunteer.jpg", id:7},
+    {name: "Dress to Impress", price: "$100", image: "images/suit.jpg" },
+    {name: "Confidence Coaching", price: "$100", image: "images/confidence.jpg"},
+    {name: "Match-making", price: "$100", image: "images/matchmaking.jpg" },
+    {name: "Financial Advice", price: "$100", image: "images/money.jpg"},
+    {name: "Networking/Mentoring", price: "$100" , image: "images/networking.jpg"},
+    {name: "Shaving Kit", price: "$100", image: "images/shaving.jpg"},
+    {name: "Give Back", price: "$0", image: "images/volunteer.jpg"},
 ];
    
 var productHtml = "";
 for (i = 0; i < products.length; i++) { 
-    productHtml += `<div id="${products[i].id}" class="indiv-prod">`;
-    productHtml += `<img id="pics_${products[i].id}" src="${products[i].image}"/>`;
+    productHtml += `<div class="indiv-prod">`;
+    productHtml += `<img src="${products[i].image}"/>`;
     productHtml += `<h2>${products[i].name}</h2>`;
     productHtml += `<h4>${products[i].price}</h4>`;
     productHtml += `<button class="shopping-cart">Add to Cart</button>`
     productHtml += `</div>`;
 }
-var productElement = document.getElementById("card");
+document.getElementById("card").innerHTML = productHtml;
 
-productElement.innerHTML = productHtml;
-productElement.addEventListener("click",redirect);
-
-function redirect(event) {
-    
-    if (event.target.className === "shopping-cart") {
-        window.location="shopping-cart.html";
-
-    } else if (event.target.id === "pics_1") {  
-        window.location="pro-info.html";
-    }      
-};
-
-
+var prodPageLink = product[0].images;
+prodPageLink = document.getElementById("suit").innerHTML;
